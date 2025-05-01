26dbf8190b20e1d03cb4c2d78aeeaba947c84b54
Srashti Rai <141318435+srashti-19@users.noreply.github.com>
2025-05-01 09:59:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,12 +6,10 @@ import connectCloudinary from "./config/cloudinary.js";
 import companyRoutes from "./routes/companyRoutes.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-// import { clerkMiddleware } from "@clerk/express";
+import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/webhooks.js";
 import * as Sentry from "@sentry/node";
 import "./config/instrument.js";
-// add
- import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 // Initialize Express
 const app = express();
@@ -36,7 +34,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 // add middlerware
-app.use("/api/users", requireAuth(),userRoutes);
+app.use("/api/users", userRoutes);
 app.post("/webhooks", clerkWebhooks);
 app.use("/api/company", companyRoutes);
 app.use("/api/jobs", jobRoutes);
